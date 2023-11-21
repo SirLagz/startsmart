8899cf505f6f8720d9d6e170c393eb84e540c19a
SirLagz <sirlagz@gmail.com>
2023-11-21 14:42:28 +0800

    Updated with SMART stats

--- startsmart.sh
+++ startsmart.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+if [[ $(which smartctl) ]]; then
 drive=$1
 echo $drive
 blksize=$(lsblk -o phy-sec /dev/$drive | grep -vi phy-sec  | uniq | xargs)
@@ -7,15 +8,31 @@ echo Blocksize $blksize
 echo Serial $serial
 length=$(expr length $serial)
 if [ ! -f $serial-smart ] && [ $length -gt 0 ]; then
-    echo "smartctl -i /dev/$drive > $serial"
+    echo "smartctl -i /dev/$drive > $serial-smart"
     smartctl -i /dev/$drive > $serial-smart
-    echo "badblocks -b $blksize -c 65536 -swv /dev/$drive -o $serial-badblocks -p 2"
-    screen -S $drive -L $serial-$drive-log -d -m /sbin/badblocks -b $blksize -c 65535 -wv /dev/$drive -o $serial-badblocks -p 2
+    if [[ $(smartctl -A /dev/$drive | grep -E '^\s+5|^187|^188|^189|^197|^198' | awk -F ' ' ' { print $10 } ' | grep -v 0 | wc -l) -gt 0 ]]; then 
+        echo 'SMART attributes failed. Please check below output.'
+        smartctl -A /dev/$drive | grep -E '^\s+5|^187|^188|^189|^197|^198' | awk -F ' ' ' { print $1 " - " $2 " - " $10 } '
+    else
+        echo 'SMART attributes passed'
+    fi
+
+    echo "/sbin/badblocks -b $blksize -c 65536 -wv /dev/$drive -o $serial-badblocks"
+    screen -S $drive -L -Logfile $serial-$drive-log -d -m /sbin/badblocks -b $blksize -c 65535 -wv /dev/$drive -o $serial-badblocks
 else
     if [[ $length -eq 0 ]]; then
         echo "Serial number invalid, stopping"
     else
         echo "Drive scanned already. stopping"
-        ./get-badblocks.sh $serial
+        if [[ $(smartctl -A /dev/$drive | grep -E '^\s+5|^187|^188|^189|^197|^198' | awk -F ' ' ' { print $10 } ' | grep -v 0 | wc -l) -gt 0 ]]; then 
+            echo 'SMART attributes failed. Please check below output.'
+            smartctl -A /dev/$drive | grep -E '^\s+5|^187|^188|^189|^197|^198' | awk -F ' ' ' { print $1 " - " $2 " - " $10 } '
+        else
+            echo 'SMART attributes passed'
+        fi
+	./get-badblocks.sh $serial
     fi
+fi
+else
+echo "smartctl not found. Aborting"
 fi
